3004865a22b7d824f312bdba3ef95ee607afa788
Thanarat Pongjan <siratat59@gmail.com>
2022-11-26 11:24:53 +0700

    workshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello Kitty');
+  res.status(200).json({
+    fullname: 'Thanarat Pongjan'
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  //res.send('Hello Kitty');
+  res.status(200).json({
+    fullname: 'Thanarat Pongjan',
+    nickname: 'Ikkyu',
+    hobby: 'Travel',
+    gitusername: 'minicycle'
+  })
 });
 
 module.exports = router;
