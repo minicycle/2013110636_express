d03747a058392e9781107e6f1b4f8f717136fc4b
Thanarat Pongjan <siratat59@gmail.com>
2022-12-10 16:17:40 +0700

    WorkShopB

--- routes/company.js
+++ routes/company.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 const companyColltroller = require('../controllers/companyColltroller')
 
-router.get('/', companyColltroller.comp);
+router.get('/', companyColltroller.comp); //get
+
+router.get('/:id', companyColltroller.show); //get by id
+router.post('/', companyColltroller.insert); //insert
+router.delete('/:id', companyColltroller.destroy); //delete
+router.put('/:id', companyColltroller.update); //update
 
 module.exports = router;
