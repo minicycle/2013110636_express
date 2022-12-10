3b10e421e1c51d1125ddbdc77ef2acf21cd2e1cd
Thanarat Pongjan <siratat59@gmail.com>
2022-12-10 11:46:47 +0700

    WorkShopA

--- controllers/companyColltroller.js
+++ controllers/companyColltroller.js
@@ -1,31 +1,9 @@
-exports.comp = (req, res, next) => {
+const Company = require('../models/company')
+exports.comp = async(req, res, next) => {
     
+    const company = await Company.findOne()
+
     res.status(200).json({
-      data:[
-        {
-            id:1,
-            name:"Charoen Pokphand Group",
-            address: {
-                province: "Bangkok",
-                postcode: 10220
-            }
-        },
-        {
-            id:2,
-            name:"Bangkok Dock Co., (1957) Ltd.",
-            address: {
-                province: "Bangkok",
-                postcode: 10120
-            }
-        },
-        {
-            id:3,
-            name:"Bangkok Synthetics Co.,LTD.",
-            address: {
-                province: "Bangkok",
-                postcode: 10120
-            }
-        }
-      ]
+      data: company
     })
   }
