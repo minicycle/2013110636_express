301079ad913510987fab88fa2f89347e3fc218d6
Thanarat Pongjan <siratat59@gmail.com>
2023-02-04 11:32:41 +0700

    Final

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.status(200).json({
-    message: 'RESTFul API Final Class_'
+    message: 'RESTFul API Final Class'
   })
 });
 
